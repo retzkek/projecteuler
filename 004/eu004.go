c1b292fb45ce2440ce5ada496afd2d16aeb6f117
Kevin Retzke <retzkek@gmail.com>
2012-04-10 17:10:54 -0500

    completed Go solution to problem 004

--- 004/eu004.go
+++ 004/eu004.go
@@ -5,20 +5,17 @@ package main
 import (
 	"fmt"
     "math"
+    "strconv"
 )
-
-// Reverse reverses and integer, e.g. 123 returns 321.
-func reverse(num int) int {
-    var digits, result int
-    for digits=1;int(math.Pow10(digits))<num;digits++ {}
-    for i:=1;i<=digits;i++ {
-        result += (num%int(math.Pow10(i)))*int(math.Pow10(digits-i))
-    }
-    return result
-}
 
 // IsPalindrome returns true if num is palindromic.
 func isPalindrome(num int) bool {
+    str := strconv.Itoa(num)
+    for i:=0;i<len(str)/2;i++ {
+        if str[i] != str[len(str)-i-1] {
+            return false
+        }
+    }
     return true
 }
 
@@ -44,7 +41,6 @@ func palindrome(digits int) (int, int, int) {
 
 func main() {
 	var a, b, r int
-    fmt.Println(1234,reverse(1234))
 	// test case
 	if a,b,r = palindrome(2); r == 9009 {
 		fmt.Println("Test: pass")
@@ -52,6 +48,6 @@ func main() {
 		fmt.Printf("Test: fail (r=%v (%v * %v))\n", r, a, b)
 	}
 	// challenge
-	//a,b,r = palindrome(3)
-	//fmt.Printf("Result: %v (%i * %i)\n", r, a, b)
+	a,b,r = palindrome(3)
+	fmt.Printf("Result: %v (%v * %v)\n", r, a, b)
 }
