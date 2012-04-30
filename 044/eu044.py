babedcfa634df46787b20dc728bc7806322b7235
Kevin Retzke <retzkek@gmail.com>
2012-04-30 00:40:06 -0500

    started problem 44

---
+++ 044/eu044.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+project euler (projecteuler.net) problem 44
+solution by Kevin Retzke (retzkek@gmail.com), April 2012
+"""
+
+def pentagonal(n):
+    return n*(3*n-1)/2
+
+if __name__ == "__main__":
+    ps = []
+    for i in xrange(1,100000):
+        ps.append(pentagonal(i))
+    pset = set(ps)
+    for i in range(len(ps)):
+        for j in range(i,len(ps)):
+            m = ps[i]
+            n = ps[j]
+            if n != m and (m+n) in pset and (m-n) in pset:
+                print m, n, abs(m-n)
