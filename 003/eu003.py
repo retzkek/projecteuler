08f7a04de10bb0d1deebad9081822d788d4e765a
Kevin Retzke <retzkek@gmail.com>
2012-05-01 22:20:58 -0500

    added README

--- 003/eu003.py
+++ 003/eu003.py
@@ -4,10 +4,6 @@
 project euler (projecteuler.net) problem 3:
 solution by Kevin Retzke (retzkek@gmail.com), April 2009
 NOTE: THIS IS THEORETICALLY A VALID SOLUTION, BUT IT WILL NOT RUN DUE TO MEMORY REQUIREMENTS
-
-The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
 """
 
 from math import sqrt
--- 003/eu003a.py
+++ 003/eu003a.py
@@ -3,10 +3,6 @@
 """
 project euler (projecteuler.net) problem 3:
 solution by Kevin Retzke (retzkek@gmail.com), April 2009
-
-The prime factors of 13195 are 5, 7, 13 and 29.
-
-What is the largest prime factor of the number 600851475143 ?
 """
 
 def primefacs(num):
