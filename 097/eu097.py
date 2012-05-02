1085748cce94e7498a9f07e5bd6d340043891e3e
Kevin Retzke <retzkek@gmail.com>
2012-05-02 16:24:46 -0500

    added python solution to problem 97

---
+++ 097/eu097.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+project euler (projecteuler.net) problem 97
+solution by Kevin Retzke (retzkek@gmail.com), May 2012
+"""
+if __name__ == "__main__":
+    print (28433*(2**7830457)+1)%10**10
