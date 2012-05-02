56038a97730f12353ef83b260eea097b9199f539
Kevin Retzke <retzkek@gmail.com>
2012-05-01 22:42:25 -0500

    added go solution to problem 5

---
+++ 005/eu005.go
@@ -0,0 +1,41 @@
+// project euler (projecteuler.net) problem 5:
+// solution by Kevin Retzke (retzkek@gmail.com), April 2009
+package main
+
+import (
+    "fmt"
+)
+
+func smallestDivisibleBy(max int) int {
+    // calculates the smallest number that is evenly divisible by all 
+    // integers from 1 to max (inclusive)
+    i := 0
+    found := false
+    for !found {
+        i += max
+        candidate := true
+        for j := max; j > max/2-1; j-- {
+            if i % j != 0 {
+                candidate = false
+                break
+            }
+        }
+        if candidate {
+            found = true
+        }
+    }
+    return i
+}
+
+func main() {
+    // test case
+    r := smallestDivisibleBy(10)
+    if r == 2520 {
+        fmt.Println("Test: pass")
+    } else {
+        fmt.Printf("Test: fail (result = %v)\n",r)
+    }
+    // challenge
+    r = smallestDivisibleBy(20)
+    fmt.Printf("Result: %v\n",r)
+}
