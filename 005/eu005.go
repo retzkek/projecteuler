8e7c4f2c01b5906fe9aade70c0d6d35463356d66
Kevin Retzke <retzkek@gmail.com>
2012-05-01 22:44:22 -0500

    ran through gofmt

--- 005/eu005.go
+++ 005/eu005.go
@@ -3,39 +3,39 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
+// Calculates the smallest number that is evenly divisible by all 
+// integers from 1 to max (inclusive).
 func smallestDivisibleBy(max int) int {
-    // calculates the smallest number that is evenly divisible by all 
-    // integers from 1 to max (inclusive)
-    i := 0
-    found := false
-    for !found {
-        i += max
-        candidate := true
-        for j := max; j > max/2-1; j-- {
-            if i % j != 0 {
-                candidate = false
-                break
-            }
-        }
-        if candidate {
-            found = true
-        }
-    }
-    return i
+	i := 0
+	found := false
+	for !found {
+		i += max
+		candidate := true
+		for j := max; j > max/2-1; j-- {
+			if i%j != 0 {
+				candidate = false
+				break
+			}
+		}
+		if candidate {
+			found = true
+		}
+	}
+	return i
 }
 
 func main() {
-    // test case
-    r := smallestDivisibleBy(10)
-    if r == 2520 {
-        fmt.Println("Test: pass")
-    } else {
-        fmt.Printf("Test: fail (result = %v)\n",r)
-    }
-    // challenge
-    r = smallestDivisibleBy(20)
-    fmt.Printf("Result: %v\n",r)
+	// test case
+	r := smallestDivisibleBy(10)
+	if r == 2520 {
+		fmt.Println("Test: pass")
+	} else {
+		fmt.Printf("Test: fail (result = %v)\n", r)
+	}
+	// challenge
+	r = smallestDivisibleBy(20)
+	fmt.Printf("Result: %v\n", r)
 }
