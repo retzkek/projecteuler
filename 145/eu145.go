d39948c87de0c07532e148df2a223bfa9dee6dc7
Kevin Retzke <retzkek@gmail.com>
2012-05-04 19:50:05 -0500

    ran through gofmt

--- 145/eu145.go
+++ 145/eu145.go
@@ -8,7 +8,7 @@ import (
 
 // Reverse returns the reverse of n, e.g. Reverse(123)=321.
 func Reverse(n int) int {
-    // compute reverse of n
+	// compute reverse of n
 	rev := 0
 	for ; n > 0; n = n / 10 {
 		rev = rev*10 + (n % 10)
@@ -19,28 +19,28 @@ func Reverse(n int) int {
 // IsReversible tests if n is reversible, i.e. n+reverse(n) contains
 // all odd digits.
 func IsReversible(n int) bool {
-    // reverse(n) cannot have leading zeros
-    if n%10 == 0 {
-        return false
-    }
-    // compute reverse of n
+	// reverse(n) cannot have leading zeros
+	if n%10 == 0 {
+		return false
+	}
+	// compute reverse of n
 	rev := Reverse(n)
-    // test if digits are odd
-    for m := n+rev; m > 0; m = m / 10 {
-        if (m%10)%2 == 0 {
-            return false
-        }
-    }
+	// test if digits are odd
+	for m := n + rev; m > 0; m = m / 10 {
+		if (m%10)%2 == 0 {
+			return false
+		}
+	}
 	return true
 }
 
 func main() {
 	result := 0
 	for n := 1; n < 1000000000; n++ {
-        //fmt.Println(n,Reverse(n))
-        if IsReversible(n) {
-            result++
-        }
+		//fmt.Println(n,Reverse(n))
+		if IsReversible(n) {
+			result++
+		}
 	}
 	fmt.Println(result)
 }
