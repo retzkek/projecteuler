bbaba5f85605686719535c42c2c812504e5c50ab
Kevin Retzke <retzkek@gmail.com>
2012-05-06 02:23:10 -0500

    ran through gofmt

--- 058/eu058.go
+++ 058/eu058.go
@@ -3,15 +3,15 @@
 package main
 
 import (
-    "math"
-    "fmt"
+	"fmt"
+	"math"
 )
 
 // Primes is a type for containing a list of primes.
 type Primes struct {
-	Primes []int
-    PrimeSet map[int] bool
-	Last   int
+	Primes   []int
+	PrimeSet map[int]bool
+	Last     int
 }
 
 // Init initializes a Primes struct with the first two primes.
@@ -43,7 +43,7 @@ func (p *Primes) Next() int {
 		}
 	}
 	p.Primes = append(p.Primes, next)
-    p.PrimeSet[next] = true
+	p.PrimeSet[next] = true
 	p.Last = next
 	return next
 }
@@ -51,51 +51,51 @@ func (p *Primes) Next() int {
 // Eratosthenes populates p with all primes up to max, computed with
 // the Sieve of Eratosthenes.
 func (p *Primes) Eratosthenes(max int) {
-    sieve := make([]bool, max)
-    for i := 2; i*i < max; i++ {
-        if !sieve[i] {
-            for j := 2*i; j < max; j += i {
-                sieve[j] = true
-            }
-        }
-    }
+	sieve := make([]bool, max)
+	for i := 2; i*i < max; i++ {
+		if !sieve[i] {
+			for j := 2 * i; j < max; j += i {
+				sieve[j] = true
+			}
+		}
+	}
 	p.Primes = []int{2}
-    p.PrimeSet = make(map[int] bool)
-    p.PrimeSet[2] = true
-    for i := 3; i < max; i++ {
-        if !sieve[i] {
-            p.Last = i
-            p.Primes = append(p.Primes, p.Last)
-            p.PrimeSet[p.Last] = true
-        }
-    }
+	p.PrimeSet = make(map[int]bool)
+	p.PrimeSet[2] = true
+	for i := 3; i < max; i++ {
+		if !sieve[i] {
+			p.Last = i
+			p.Primes = append(p.Primes, p.Last)
+			p.PrimeSet[p.Last] = true
+		}
+	}
 }
 
 func (p *Primes) IsPrime(n int) bool {
-    for n > p.Last {
-        p.Next()
-    }
-    return p.PrimeSet[n]
+	for n > p.Last {
+		p.Next()
+	}
+	return p.PrimeSet[n]
 }
 
 func main() {
-    p := new(Primes)
-    p.Eratosthenes(100000000)
-    side := 3
-    nprime := 3.0
-    ndiag := 5.0
-    lastn := 9
-    for nprime/ndiag > 0.1 {
-        i := (side-1)/2+1
-        for j := 0; j < 4; j++ {
-            lastn = lastn+2*i
-            if p.IsPrime(lastn) {
-                nprime += 1.0
-            }
-        }
-        ndiag += 4.0
-        side += 2
-        //fmt.Println(side, nprime/ndiag, lastn)
-    }
-    fmt.Println(side)
+	p := new(Primes)
+	p.Eratosthenes(100000000)
+	side := 3
+	nprime := 3.0
+	ndiag := 5.0
+	lastn := 9
+	for nprime/ndiag > 0.1 {
+		i := (side-1)/2 + 1
+		for j := 0; j < 4; j++ {
+			lastn = lastn + 2*i
+			if p.IsPrime(lastn) {
+				nprime += 1.0
+			}
+		}
+		ndiag += 4.0
+		side += 2
+		//fmt.Println(side, nprime/ndiag, lastn)
+	}
+	fmt.Println(side)
 }
