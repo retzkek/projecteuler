1c4620edfa472294149d79b0e12d3b8cbd698555
Kevin Retzke <kevin.retzke@transware.net>
2012-05-08 17:52:12 -0500

    fixed concurrent go case (was using same struct in multiple goroutines)

--- 070/eu070.go
+++ 070/eu070.go
@@ -3,116 +3,112 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 type Primes struct {
-    Primes []int
-    Last   int
+	Primes []int
+	Last   int
 }
 
 // Init initializes a Primes struct with the first two primes.
 func (p *Primes) Init() {
-    p.Primes = []int{2, 3}
-    p.Last = 3
+	p.Primes = []int{2, 3}
+	p.Last = 3
 }
 
 // Next computes, returns, and appends the next prime number.
 func (p *Primes) Next() int {
-    next := 0
-    i := p.Last + 2
-    for next == 0 {
-        sqrti := math.Sqrt(float64(i))
-        isPrime := true
-        for _, p := range p.Primes {
-            if i%p == 0 {
-                isPrime = false
-                i += 2
-                break
-            }
-            if float64(p) > sqrti {
-                break
-            }
-        }
-        if isPrime {
-            next = i
-        }
-    }
-    p.Primes = append(p.Primes, next)
-    p.Last = next
-    return next
+	next := 0
+	i := p.Last + 2
+	for next == 0 {
+		sqrti := math.Sqrt(float64(i))
+		isPrime := true
+		for _, p := range p.Primes {
+			if i%p == 0 {
+				isPrime = false
+				i += 2
+				break
+			}
+			if float64(p) > sqrti {
+				break
+			}
+		}
+		if isPrime {
+			next = i
+		}
+	}
+	p.Primes = append(p.Primes, next)
+	p.Last = next
+	return next
 }
 
 // Eratosthenes populates p with all primes up to max, computed with
 // the Sieve of Eratosthenes.
 func (p *Primes) Eratosthenes(max int) {
-    sieve := make([]bool, max)
-    for i := 2; i*i < max; i++ {
-        if !sieve[i] {
-            for j := 2 * i; j < max; j += i {
-                sieve[j] = true
-            }
-        }
-    }
-    p.Primes = []int{2}
-    for i := 3; i < max; i++ {
-        if !sieve[i] {
-            p.Last = i
-            p.Primes = append(p.Primes, p.Last)
-        }
-    }
+	sieve := make([]bool, max)
+	for i := 2; i*i < max; i++ {
+		if !sieve[i] {
+			for j := 2 * i; j < max; j += i {
+				sieve[j] = true
+			}
+		}
+	}
+	p.Primes = []int{2}
+	for i := 3; i < max; i++ {
+		if !sieve[i] {
+			p.Last = i
+			p.Primes = append(p.Primes, p.Last)
+		}
+	}
 }
 
 func countDigits(n int) []int {
-    digits := make([]int,10)
-    for n > 0 {
-        digits[n%10]++
-        n = n/10
-    }
-    return digits
+	digits := make([]int,10)
+	for n > 0 {
+		digits[n%10]++
+		n = n/10
+	}
+	return digits
 }
 
 func arePermutations(n, m int) bool {
-    // test order of magnitude
-    //if int(math.Log10(float64(n))) != int(math.Log10(float64(m))) {
-    //    return false
-    //}
-    nlist := countDigits(n)
-    mlist := countDigits(m)
-    for d := 0; d < 10; d++ {
-        if nlist[d] != mlist[d] {
-            return false
-        }
-    }
-    return true
+	// test order of magnitude
+	//if int(math.Log10(float64(n))) != int(math.Log10(float64(m))) {
+	//	return false
+	//}
+	nlist := countDigits(n)
+	mlist := countDigits(m)
+	for d := 0; d < 10; d++ {
+		if nlist[d] != mlist[d] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    const maxn = 10000000
-    primes := new(Primes)
-    primes.Eratosthenes(maxn)
-    minn := 0
-    minnphin := float64(9999999)
-    for i,p := range primes.Primes {
-        for j := i; j < len(primes.Primes); j++ {
-            q := primes.Primes[j]
-            n := p*q
-            phi := float64(n)
-            for _, m := range primes.Primes {
-                if m > n {
-                    break
-                }
-                if n%m == 0 {
-                    phi *= (1 - 1/float64(m))
-                }
-            }
-            if nphin := float64(n) / phi; nphin < minnphin && 
-                arePermutations(n,int(phi)) {
-                minn = n
-                minnphin = nphin
-                fmt.Println(minn,int(phi), minnphin)
-            }
-        }
-    }
+	const maxn = 10000000
+	primes := new(Primes)
+	primes.Eratosthenes(maxn)
+	minn := 0
+	minnphin := float64(9999999)
+	for n := 3; n < maxn; n+=2 {
+		phi := float64(n)
+		for _, m := range primes.Primes {
+			if m > n {
+				break
+			}
+			if n%m == 0 {
+				phi *= (1 - 1/float64(m))
+			}
+		}
+		if nphin := float64(n) / phi; nphin < minnphin && 
+			arePermutations(n,int(phi)) {
+			minn = n
+			minnphin = nphin
+			fmt.Println(minn,int(phi), minnphin)
+		}
+	}
 }
--- 070/eu070c.go
+++ 070/eu070c.go
@@ -3,128 +3,134 @@
 package main
 
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
+const NCPU = 15
+
 type Primes struct {
-    Primes []int
-    Last   int
+	Primes []int
+	Last   int
 }
 
 // Init initializes a Primes struct with the first two primes.
 func (p *Primes) Init() {
-    p.Primes = []int{2, 3}
-    p.Last = 3
+	p.Primes = []int{2, 3}
+	p.Last = 3
 }
 
 // Next computes, returns, and appends the next prime number.
 func (p *Primes) Next() int {
-    next := 0
-    i := p.Last + 2
-    for next == 0 {
-        sqrti := math.Sqrt(float64(i))
-        isPrime := true
-        for _, p := range p.Primes {
-            if i%p == 0 {
-                isPrime = false
-                i += 2
-                break
-            }
-            if float64(p) > sqrti {
-                break
-            }
-        }
-        if isPrime {
-            next = i
-        }
-    }
-    p.Primes = append(p.Primes, next)
-    p.Last = next
-    return next
+	next := 0
+	i := p.Last + 2
+	for next == 0 {
+		sqrti := math.Sqrt(float64(i))
+		isPrime := true
+		for _, p := range p.Primes {
+			if i%p == 0 {
+				isPrime = false
+				i += 2
+				break
+			}
+			if float64(p) > sqrti {
+				break
+			}
+		}
+		if isPrime {
+			next = i
+		}
+	}
+	p.Primes = append(p.Primes, next)
+	p.Last = next
+	return next
 }
 
 // Eratosthenes populates p with all primes up to max, computed with
 // the Sieve of Eratosthenes.
 func (p *Primes) Eratosthenes(max int) {
-    sieve := make([]bool, max)
-    for i := 2; i*i < max; i++ {
-        if !sieve[i] {
-            for j := 2 * i; j < max; j += i {
-                sieve[j] = true
-            }
-        }
-    }
-    p.Primes = []int{2}
-    for i := 3; i < max; i++ {
-        if !sieve[i] {
-            p.Last = i
-            p.Primes = append(p.Primes, p.Last)
-        }
-    }
+	sieve := make([]bool, max)
+	for i := 2; i*i < max; i++ {
+		if !sieve[i] {
+			for j := 2 * i; j < max; j += i {
+				sieve[j] = true
+			}
+		}
+	}
+	p.Primes = []int{2}
+	for i := 3; i < max; i++ {
+		if !sieve[i] {
+			p.Last = i
+			p.Primes = append(p.Primes, p.Last)
+		}
+	}
 }
 
 func countDigits(n int) []int {
-    digits := make([]int,10)
-    for n > 0 {
-        digits[n%10]++
-        n = n/10
-    }
-    return digits
+	digits := make([]int, 10)
+	for n > 0 {
+		digits[n%10]++
+		n = n / 10
+	}
+	return digits
 }
 
 func arePermutations(n, m int) bool {
-    // test order of magnitude
-    //if int(math.Log10(float64(n))) != int(math.Log10(float64(m))) {
-    //    return false
-    //}
-    nlist := countDigits(n)
-    mlist := countDigits(m)
-    for d := 0; d < 10; d++ {
-        if nlist[d] != mlist[d] {
-            return false
-        }
-    }
-    return true
+	// test order of magnitude
+	//if int(math.Log10(float64(n))) != int(math.Log10(float64(m))) {
+	//    return false
+	//}
+	nlist := countDigits(n)
+	mlist := countDigits(m)
+	for d := 0; d < 10; d++ {
+		if nlist[d] != mlist[d] {
+			return false
+		}
+	}
+	return true
 }
 
 type Result struct {
-    N int
-    Phi float64
-    NOverPhi float64
+	N        int
+	Phi      float64
+	NOverPhi float64
+}
+
+func totient(n int, p *Primes, c chan Result) {
+	r := Result{n, float64(n), 1.0}
+	for _, m := range p.Primes {
+		if m > n {
+			break
+		}
+		if n%m == 0 {
+			r.Phi *= (1 - 1/float64(m))
+		}
+	}
+	r.NOverPhi = float64(n) / r.Phi
+	c <- r
 }
 
 func main() {
-    const MAXN = 10000000
-    const NCPU = 4
-    p := new(Primes)
-    p.Eratosthenes(MAXN)
-    result := Result{0,0.0,999999.0}
-    c := make(chan Result)
-    //for _,n := range p.Primes {
-    for n := 3; n <= MAXN; n += 2*NCPU {
-        for nn := n; nn < n+2*NCPU; nn += 2 {
-            go func() {
-                r := Result{nn, float64(nn), 1.0}
-                for _, m := range p.Primes {
-                    if m > nn {
-                        break
-                    }
-                    if nn%m == 0 {
-                        r.Phi *= (1 - 1/float64(m))
-                    }
-                }
-                r.NOverPhi = float64(nn)/r.Phi
-                c <- r
-            }()
-        }
-        for i := 0; i < NCPU; i++ {
-            r := <- c
-            if r.NOverPhi < result.NOverPhi && 
-                arePermutations(r.N,int(r.Phi)) {
-                result = r
-                fmt.Println(result.N,int(result.Phi), result.NOverPhi)
-            }
-        }
-    }
+	const MAXN = 10000000
+	p := new(Primes)
+	p.Eratosthenes(MAXN)
+	result := Result{0, 0.0, 999999.0}
+	c := make(chan Result)
+	for n := 3; n <= MAXN; n += 2 * NCPU {
+		for nn := n; nn < n+2*NCPU; nn += 2 {
+			go totient(nn, p, c)
+		}
+		//fmt.Println("----------------")
+		for i := 0; i < NCPU; i++ {
+			r := <-c
+			//fmt.Println(r)
+			if r.NOverPhi < result.NOverPhi &&
+				arePermutations(r.N, int(r.Phi)) {
+				result.N = r.N
+				result.Phi = r.Phi
+				result.NOverPhi = r.NOverPhi
+				fmt.Println(result)
+			}
+		}
+	}
 }
