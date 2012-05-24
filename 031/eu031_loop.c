211e3ab7e60c5c0640cbb421b70452bc7db7fdc1
Kevin Retzke <retzkek@gmail.com>
2012-05-23 22:25:32 -0500

    added loop version

---
+++ 031/eu031_loop.c
@@ -0,0 +1,21 @@
+// project euler (projecteuler.net) problem 31
+// solution by Kevin Retzke (retzkek@gmail.com), May 2012
+#include <stdio.h>
+
+int countCombinations(const int *val, const int *end, int total) 
+{
+	if (val > end) {
+        return total == 0;
+	}
+	int count = 0;
+    for (int i = 0; i <= total/(*val); ++i) {
+		count += countCombinations(val+1, end, total-(*val)*i);
+	}
+	return count;
+}
+
+int main()
+{
+	static const int coins[] = {200, 100, 50, 20, 10, 5, 2, 1};
+	printf("%i\n",countCombinations(&coins[0], &coins[7], 200));
+}
