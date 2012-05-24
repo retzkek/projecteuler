c2a4549762ecb5453cd5b6834ef974dfb5eff60d
Kevin Retzke <retzkek@gmail.com>
2012-05-24 00:30:32 -0500

    fixed indents

--- 031/eu031_loop.c
+++ 031/eu031_loop.c
@@ -4,18 +4,18 @@
 
 int countCombinations(const int *val, const int *end, int total) 
 {
-	if (val > end) {
+    if (val > end) {
         return total == 0;
-	}
-	int count = 0;
+    }
+    int count = 0;
     for (int i = 0; i <= total/(*val); ++i) {
-		count += countCombinations(val+1, end, total-(*val)*i);
-	}
-	return count;
+        count += countCombinations(val+1, end, total-(*val)*i);
+    }
+    return count;
 }
 
 int main()
 {
-	static const int coins[] = {200, 100, 50, 20, 10, 5, 2, 1};
-	printf("%i\n",countCombinations(&coins[0], &coins[7], 200));
+    static const int coins[] = {200, 100, 50, 20, 10, 5, 2, 1};
+    printf("%i\n",countCombinations(&coins[0], &coins[7], 200));
 }
