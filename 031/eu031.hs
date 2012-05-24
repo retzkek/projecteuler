7478bad250242e2748c14e0614ede0e85fc126a5
Kevin Retzke <retzkek@gmail.com>
2012-05-23 23:29:39 -0500

    added haskell solution to problem 31

---
+++ 031/eu031.hs
@@ -0,0 +1,16 @@
+-- project euler (projecteuler.net) problem 31
+-- solution by Kevin Retzke (retzkek@gmail.com), May 2012
+
+main = do
+    putStrLn (show (count [200,100,50,20,10,5,2,1] 200))
+
+count :: [Int] -> Int -> Int
+
+count (x:xs) total 
+    | x <= total = a+b
+    | otherwise  = b
+    where a = count (x:xs) (total-x)
+          b = count xs total
+count [] 0 = 1
+count [] _ = 0
+
