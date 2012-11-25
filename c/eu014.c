b41174161bb0eed84f29f40076c19cf64b70b8ba
Kevin Retzke <retzkek@gmail.com>
2012-11-25 01:51:47 -0600

    added c solution to problem 14

---
+++ c/eu014.c
@@ -0,0 +1,66 @@
+// project euler (projecteuler.net) problem 14
+// solution by Kevin Retzke (retzkek@gmail.com)
+// November 2012
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "pe.h"
+
+#define MAX_N 1000000
+
+int countn[MAX_N];
+
+long next(long n)
+{
+	if (n%2 == 0) {
+		return n/2;
+	} else {
+		return 3*n+1;
+	}
+}
+
+int count(int n)
+{
+	int i = 1;
+	long m = n;
+	while (m > 1) {
+		i++;
+		m = next(m);
+		if (m < MAX_N) {
+			if (countn[m] > 0) {
+				i += countn[m];
+				break;
+			}
+		}
+	}
+	countn[n] = i;
+	return i;
+}
+
+bool test()
+{
+	return (count(13) == 10);
+}
+
+void run()
+{
+	long m, max;
+	int imax;
+	max = 0;
+	for (int i = 1; i < MAX_N; i++) {
+		m = count(i);
+		if (m > max) {
+			max = m;
+			imax = i;
+		}
+	}
+	printf("%d\n",imax);
+}
+
+int main()
+{
+	memset(&countn[0], 0, MAX_N);
+	return projectEuler(test,run);
+}
