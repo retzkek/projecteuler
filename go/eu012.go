42603e8682e65ef099cc6b92c6288601fc55b7d6
Kevin Retzke <retzkek@gmail.com>
2012-12-15 01:20:12 -0600

    added go solution to problem 12

---
+++ go/eu012.go
@@ -0,0 +1,53 @@
+// project euler (projecteuler.net) problem 12
+// solution by Kevin Retzke (retzkek@gmail.com)
+// November 2012
+package main
+
+import (
+	"fmt"
+)
+
+func triangle(n int) int {
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	return t
+}
+
+func nFactors(n int) int {
+	r := 0
+	i := 1
+	for i*i < n {
+		if n%i == 0 {
+			r += 2
+		}
+		i++
+	}
+	return r
+}
+
+func firstOverNFactors(n int) int {
+	var t int
+	for i := 0; ; i++ {
+		t = triangle(i)
+		tf := nFactors(t)
+		if tf > n {
+			break
+		}
+	}
+	return t
+}
+
+func main() {
+	if triangle(7) != 28 {
+		fmt.Printf("Test failed. triangle(7) = %d (should be 28)\n", triangle(7))
+		return
+	}
+	if firstOverNFactors(5) != 28 {
+		fmt.Printf("Test failed. firstOverNFactors(5) = %d (should be 28)\n",
+			firstOverNFactors(5))
+		return
+	}
+	fmt.Println(firstOverNFactors(500))
+}
