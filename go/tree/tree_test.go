3f818397b231219fd0de328107747709df103071
Kevin Retzke <retzkek@gmail.com>
2012-12-18 12:44:10 -0600

    ran through gofmt

--- go/tree/tree_test.go
+++ go/tree/tree_test.go
@@ -1,15 +1,15 @@
 package tree
 
 import (
-	"testing"
-	"os"
 	"fmt"
+	"os"
+	"testing"
 )
 
 func TestInt(t *testing.T) {
 	bt := NewTreeInt()
-	ns := []int{10,20,19,4,102,34}
-	for _,n := range ns {
+	ns := []int{10, 20, 19, 4, 102, 34}
+	for _, n := range ns {
 		bt.Insert(n)
 	}
 	if !bt.Contains(10) {
@@ -35,19 +35,19 @@ func TestInt(t *testing.T) {
 
 func TestString(t *testing.T) {
 	bt := NewTreeString()
-	ss := []string{"foo","bar","baz","fizz","buzz","0xDEADBEEF"}
-	for _,s := range ss {
+	ss := []string{"foo", "bar", "baz", "fizz", "buzz", "0xDEADBEEF"}
+	for _, s := range ss {
 		bt.Insert(s)
 	}
 	//bt.Graphviz(os.Stdout)
 	if v := "foo"; !bt.Contains(v) {
-		t.Errorf("Should contain %v)",v)
+		t.Errorf("Should contain %v)", v)
 	}
 	if v := "baz"; !bt.Contains(v) {
-		t.Errorf("Should contain %v)",v)
+		t.Errorf("Should contain %v)", v)
 	}
 	if v := "buzZ"; bt.Contains(v) {
-		t.Errorf("Should not contain %v)",v)
+		t.Errorf("Should not contain %v)", v)
 	}
 	if v := bt.Min(); v != "0xDEADBEEF" {
 		t.Errorf("Min should be \"0xDEADBEEF\" (is %v)", v)
@@ -57,18 +57,18 @@ func TestString(t *testing.T) {
 	}
 	bt.Remove("fizz")
 	if v := "fizz"; bt.Contains(v) {
-		t.Errorf("Should not contain %v)",v)
+		t.Errorf("Should not contain %v)", v)
 	}
 	//bt.Graphviz(os.Stdout)
 }
 
 func ExampleTree_Next() {
 	bt := NewTreeInt()
-	ns := []int{10,20,19,4,102,34}
-	for _,n := range ns {
+	ns := []int{10, 20, 19, 4, 102, 34}
+	for _, n := range ns {
 		bt.Insert(n)
 	}
-	for n := bt.First(); n != nil; n=bt.Next(n) {
+	for n := bt.First(); n != nil; n = bt.Next(n) {
 		fmt.Println(n.Item)
 	}
 	// Output:
@@ -82,8 +82,8 @@ func ExampleTree_Next() {
 
 func ExampleTree_Graphviz() {
 	bt := NewTreeInt()
-	ns := []int{10,20,19,4,102,34}
-	for _,n := range ns {
+	ns := []int{10, 20, 19, 4, 102, 34}
+	for _, n := range ns {
 		bt.Insert(n)
 	}
 	bt.Graphviz(os.Stdout)
