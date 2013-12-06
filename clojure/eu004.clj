95f1f0b3c08c9832c5d817ca01662fc6f4b1027f
Kevin Retzke <kmr@kmr.me>
2013-12-06 15:59:36 -0600

    Completed problem 4 in Clojure

--- clojure/eu004.clj
+++ clojure/eu004.clj
@@ -15,23 +15,39 @@
  (defn max-n-digit-num
    "Returns largest n-digit integer."
    [n]
-   (- (Math/round (Math/pow 10 n)) 1))
+   (dec (Math/round (Math/pow 10 n))))
  (test/is (= 9 (max-n-digit-num 1)))
  (test/is (= 99 (max-n-digit-num 2)))
  (test/is (= 999 (max-n-digit-num 3))))
 
-(defn palindrome
+(test/with-test
+ (defn min-n-digit-num
+   "Returns smallest n-digit integer."
+   [n]
+   (if (= n 1)
+     0
+     (inc (max-n-digit-num (dec n)))))
+ (test/is (= 0 (min-n-digit-num 1)))
+ (test/is (= 10 (min-n-digit-num 2)))
+ (test/is (= 100 (min-n-digit-num 3))))
+
+(defn n-digit-nums
+  "Returns all n-digit integers in reverse order."
+  [n]
+  (let [start (max-n-digit-num n)
+        stop (+ 1 (max-n-digit-num (dec n)))]
+    (range start (dec stop) -1)))
+
+(defn max-palindrome
   "Returns the largest palindrome made from two digits-digit numbers."
-  ([digits]
-   (palindrome digits 0))
-  ([digits last-max]
-   (+ last-max)))
+  [digits]
+  (apply max (for [x (n-digit-nums digits)
+                   y (n-digit-nums digits)
+                   :when (palindrome? (* x y))]
+               (* x y))))
+(def run-test '(= (max-palindrome 2) 9009))
+(def run-problem '(max-palindrome 3))
 
-(let [start (max-n-digit-num 2)
-      stop (+ 1 (max-n-digit-num 1))]
-  (range start (dec stop) -1))
-
-(test/run-tests)
-
+;(test/run-tests)
 
 
