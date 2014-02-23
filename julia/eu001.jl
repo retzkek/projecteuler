2ce6ee3a2414896d0aea902d185f8a05b0b465fb
Kevin Retzke <retzkek@gmail.com>
2014-02-22 22:29:42 -0600

    added julia solution to problem 1

---
+++ julia/eu001.jl
@@ -0,0 +1,36 @@
+#!/usr/bin/env julia
+
+# multiple determines if n is a multiple of any of the bases.
+function multiple(n::Int, bases::Vector{Int})
+    for b in bases
+        if n%b == 0
+            return true
+        end
+    end
+    return false
+end
+
+# natmult computes the sum of all multiples of the given bases that are
+# below n.
+function natmult(n::Int, bases::Vector{Int})
+    result = 0
+    for i=1:(n-1)
+        if multiple(i,bases)
+            result += i
+        end
+    end
+    result
+end
+
+bases = [3,5]
+
+# test case
+exp_r = 23
+r = natmult(10,bases)
+if r != exp_r
+    println("test failed: expected $exp_r, got $r")
+end
+
+# problem
+println(natmult(1000,bases))
+
