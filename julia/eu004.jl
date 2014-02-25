a0fa7f3976928ea801a81d22f1cae08929c5f365
Kevin Retzke <retzkek@gmail.com>
2014-02-24 21:13:14 -0600

    added julia solution to problem 4

---
+++ julia/eu004.jl
@@ -0,0 +1,35 @@
+#!/usr/bin/env julia
+
+# is_palindrome tests if n is palindromic (e.g. 12321)
+function is_palindrome(n::Int)
+    ss = string(n)
+    ss == reverse(ss)
+end
+
+# palindrome calculates the largest palindrome made from the product
+# of two d-digit numbers.
+function palindrome(d::Int)
+    start = 10^d-1
+    stop = 10^(d-1)
+    largest = 0
+    for i = start:-1:stop
+        for j = i:-1:stop
+            n = i*j
+            if n > largest && is_palindrome(n)
+                largest = n
+            end
+        end
+    end
+    largest
+end
+
+# test case
+exp_r = 9009
+r = palindrome(2)
+if r != exp_r
+    println("test failed: expected $exp_r, got $r")
+end
+
+# problem
+println(palindrome(3))
+
