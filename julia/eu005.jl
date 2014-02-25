a0a592eedffe98e6868dba6b025e897e51f350d4
Kevin Retzke <retzkek@gmail.com>
2014-02-24 21:22:44 -0600

    added julia solution to problem 5

---
+++ julia/eu005.jl
@@ -0,0 +1,32 @@
+#!/usr/bin/env julia
+
+# divisible_by_all returns the smallest integer that is evenly divisible
+# by all integers in ns.
+function divisible_by_all(ns::Array{Int})
+    i = 0
+    step = maximum(ns)
+    found = false
+    while !found
+        i += step
+        candidate = true
+        for n in ns
+            if i%n != 0
+                candidate = false
+                break
+            end
+        end
+        found = candidate
+    end
+    return i
+end
+
+# test case
+exp_r = 2520
+r = divisible_by_all([1:10])
+if r != exp_r
+    println("test failed: expected $exp_r, got $r")
+end
+
+# problem
+println(divisible_by_all([1:20]))
+
