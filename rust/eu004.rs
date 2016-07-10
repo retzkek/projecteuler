cd96811b5dc931112e70990dfeb0443fc4ca2411
Kevin Retzke <retzkek@gmail.com>
2016-07-09 23:58:27 -0500

    completed problem 4 in rust

--- rust/eu004.rs
+++ rust/eu004.rs
@@ -8,14 +8,14 @@ fn is_palindrome(s: &str) -> bool {
 }
 
 fn palindrome(digits: u32) -> u32 {
-    let base:i32 = 10;
+    let base:u32 = 10;
     let min = base.pow(digits - 1);
     let max = base.pow(digits)-1;
     let mut largest:u32 = 0;
     for i in (min..max+1).rev() {
         for j in (min..i+1).rev() {
             let n:u32 = i*j;
-            if is_palindrome(format!("{}",n)) && n > largest {
+            if is_palindrome(&format!("{}",n)) && n > largest {
                 largest = n;
                 println!("found {} ({}*{})",n,i,j);
             }
@@ -28,5 +28,6 @@ fn main() {
     assert!(!is_palindrome("foo"));
     assert!(is_palindrome("foof"));
     assert!(is_palindrome("fofof"));
-    palindrome(1);
+    assert!(palindrome(2)==9009);
+    println!("result: {}",palindrome(3));
 }
